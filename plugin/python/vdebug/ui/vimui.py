76e6c018c11f8e777521c2a25b9a33c6e44748b8
mzagol <mzsoldier@hotmail.com>
2017-04-10 13:11:48 -0500

    Added configurable watch and status window height.
    Example settings for HiDPI screen:
    let g:vdebug_options['watch_window_height']=80
    let g:vdebug_options['status_window_height']=5

--- plugin/python/vdebug/ui/vimui.py
+++ plugin/python/vdebug/ui/vimui.py
@@ -61,8 +61,8 @@ class Ui(vdebug.ui.interface.Ui):
 
             self.tracewin = TraceWindow(self,'rightbelow 7new')
 
-            self.watchwin.set_height(20)
-            self.statuswin.set_height(5)
+            self.watchwin.set_height(vdebug.opts.Options.get('watch_window_height'))
+            self.statuswin.set_height(vdebug.opts.Options.get('status_window_height'))
 
             logwin = LogWindow(self,'rightbelow 6new')
 
